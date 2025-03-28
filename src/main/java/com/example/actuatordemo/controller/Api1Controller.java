d6ec831f119945600aebcf39d9e2414cfdcffedd
Grant Banfield <gtbanfield@gmail.com>
2025-03-28 05:52:40 +0000

    test

--- src/main/java/com/example/actuatordemo/controller/Api1Controller.java
+++ src/main/java/com/example/actuatordemo/controller/Api1Controller.java
@@ -6,9 +6,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api1")
 public class Api1Controller {
 
     @Autowired
