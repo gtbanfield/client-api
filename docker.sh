298eb16f6aac58510445b89d42d04c049aee7e99
Grant <31765378+gtbanfield@users.noreply.github.com>
2025-05-21 21:51:55 +0100

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 docker build -t 192.168.1.83:5000/repository/springboot-green:v3 .
 docker tag 192.168.1.83:5000/repository/springboot-green:v2 gtbanf1979/maven:green3
-docker push 192.168.1.83:5000/repository/springboot-green:v2
 docker push gtbanf1979/maven:green3
